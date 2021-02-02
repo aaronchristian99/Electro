23e66b19cdcb514d5b38761d6f0d6127b820edbd
Aaron <aaronchristian87@gmail.com>
2021-02-02 14:43:50 -0500

    Added logo and menu to header

--- themes/electro/header.php
+++ themes/electro/header.php
@@ -27,44 +27,30 @@
 
 	<header id="masthead" class="site-header">
 		<div class="account-header grid-x red">
-			<div class="account-info cell large-2 large-offset-3 pink">Account Info</div>
+			<div class="account-info cell large-6 large-offset-6 pink">
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'electro' ); ?></button>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-primary',
+								'menu_id'        => 'primary-menu',
+							)
+						);
+						?>
+				</nav><!-- #site-navigation -->
+			</div>
 		</div>
 		<div class="logo-header grid-x blue align-middle">
-			<div class="logo cell large-2 paleblue">Logo</div>
+			<div class="logo cell large-2 paleblue">
+				<?php
+				the_custom_logo();
+				?>
+			</div>
 			<div class="search cell large-5 large-offset-2 paleblue">Search</div>
 			<div class="cart cell large-2 large-offset-1 paleblue">Cart</div>
 		</div>
 		<div class="category-header grid-x align-center pink">
 			<div class="categories cell large-8 red"></div>
 		</div>
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$electro_description = get_bloginfo( 'description', 'display' );
-			if ( $electro_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $electro_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'electro' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-primary',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
